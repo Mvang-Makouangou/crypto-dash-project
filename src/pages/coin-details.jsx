95031afdc13cb3afabefde941c51b41e1de368e9
David Mvang <makouangoudavid@gmail.com>
2025-11-13 23:13:58 +0100

    Get & Format Chart Data - Fixes #24

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router";
 import Spinner from '../components/Spinner'
+import CoinChart from "../components/CoinChart";
+
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -75,6 +77,8 @@ const CoinDetailsPage = () => {
                         </h4>
                         <h4>Last Updated: {new Date(coin.last_updated).toLocaleString()}</h4>
                      </div>
+
+                     <CoinChart  coinId={coin.id}/>
 
                       <div className='coin-details-links'>
                         {coin.links.homepage[0] && (
